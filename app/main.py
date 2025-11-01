ce83846a3b2f1f7cea8a0f966495802eff5b0e90
Anton Bublykov <mw.anton2000@gmail.com>
2025-11-01 14:50:21 +0100

    add try/except in cache decorator

--- app/main.py
+++ app/main.py
@@ -5,7 +5,13 @@ from functools import wraps
 def cache(func: Callable) -> Callable:
     cache_data = {}
     @wraps(func)
-    def wrapper(*args: Any, **kwargs: Any) -> Callable:
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        try:
+            for item in list(args) + list(kwargs.items()):
+                hash(item)
+        except TypeError:
+            return func(*args, **kwargs)
+
         key = (args, tuple(sorted(kwargs.items())))
 
         if key not in cache_data:
