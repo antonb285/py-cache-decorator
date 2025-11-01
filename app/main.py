65fcba620b52db1f9f116d627b9dd7443d52dbdc
Anton Bublykov <mw.anton2000@gmail.com>
2025-11-01 15:06:55 +0100

    improve cache decorator

--- app/main.py
+++ app/main.py
@@ -7,13 +7,11 @@ def cache(func: Callable) -> Callable:
 
     @wraps(func)
     def wrapper(*args: Any, **kwargs: Any) -> Any:
+        key = (args, tuple(sorted(kwargs.items())))
         try:
-            for item in list(args) + list(kwargs.items()):
-                hash(item)
+            hash(key)
         except TypeError:
             return func(*args, **kwargs)
-
-        key = (args, tuple(sorted(kwargs.items())))
 
         if key not in cache_data:
             print("Calculating new result")
